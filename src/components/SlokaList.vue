7eec74b8deb8ddf4cecefcb3ab7008bf231eb7c7
gaurahari-dasa <ganesh.hegde@hkm-group.org>
2025-06-21 18:11:55 +0530

    Toggle vibhagas and slokas, Haribol

--- src/components/SlokaList.vue
+++ src/components/SlokaList.vue
@@ -2,19 +2,19 @@
 import { ref } from 'vue';
 
 import Divider from './Divider.vue';
-import Vibhaga from './VibhagaNode';
 import { vibhagas } from './slokas';
 import SlokaToggle from './SlokaToggle.vue';
 
 const train = ref([]);
 
-function enumerateSlokas(vibhaga, ix, branchLevel) {
+function enumerateSlokas(vibhaga, ix) {
     let sloka = vibhaga.firstSloka;
     do {
         sloka = { ...sloka }
-        sloka.branchLevel = branchLevel + 1;
+        sloka.branchLevel = vibhaga.branchLevel;
         train.value.splice(++ix, 0, sloka)
     } while (sloka = sloka.next);
+    vibhaga.expanded = true;
 }
 
 for (let vibhaga of vibhagas) {
@@ -33,16 +33,25 @@ function expand(ix) {
     const vibhaga = { ...item.branch };
     train.value.splice(++ix, 0, vibhaga);
     vibhaga.branchLevel = item.branchLevel + 1
-    enumerateSlokas(vibhaga, ix, item.branchLevel);
+    enumerateSlokas(vibhaga, ix);
     item.expanded = true;
 }
 
-function collapse(ix) {
+function collapse(ix, branchLevel) {
     const item = train.value[ix];
-    while (train.value[ix + 1].branchLevel > item.branchLevel) {
+    while (train.value[ix + 1].branchLevel > branchLevel) {
         train.value.splice(ix + 1, 1);
     }
     item.expanded = false;
+}
+
+function toggleSloka(ix, ev) {
+    const vibhaga = train.value[ix];
+    if (ev === 'expand') {
+        enumerateSlokas(vibhaga, ix);
+    } else if (ev === 'collapse') {
+        collapse(ix, vibhaga.branchLevel - 1);
+    }
 }
 </script>
 
@@ -51,13 +60,13 @@ function collapse(ix) {
         <li v-for="(item, ix) in train" :key="`${item.chapter}_${item.sloka}`" class="sm:px-4 sm:py-4">
             <div v-if="Object.hasOwn(item, 'description')" class="relative">
                 <p>{{ item.description }}</p>
-                <SlokaToggle class="absolute bottom-0 right-0" />
+                <SlokaToggle v-show="item.branchLevel > 0" :expanded="item.expanded" class="absolute bottom-0 right-0" @click="toggleSloka(ix, $event)" />
             </div>
             <div v-else>
                 <h3 class="text-lg text-center">{{ item.devanagari }}</h3>
                 <p class="text-amber-600 mt-2 text-center">{{ item.translation }}</p>
                 <Divider class="mt-2.5" :expanded="item.expanded" v-show="item.branch"
-                    @click="item.expanded ? collapse(ix) : expand(ix)" />
+                    @click="item.expanded ? collapse(ix, item.branchLevel) : expand(ix)" />
             </div>
         </li>
     </ul>
