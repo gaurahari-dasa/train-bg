6fa70b58febdc36795e369b3e1555dceef645c16
gaurahari-dasa <ganesh.hegde@hkm-group.org>
2025-06-21 18:26:14 +0530

    Dedication to Srila Prabhupada, Haribol

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,7 @@ import SlokaList from './components/SlokaList.vue';
 
 <template>
   <main class="container mx-auto max-w-3/4">
+    <h3 class="text-sm font-semibold sticky text-center top-0 left-0">Sri Guru Gauranga Jayate - Dedicated to His Divine Grace A. C. Bhaktivedanta Swami Prabhupada</h3>
     <SlokaList />
   </main>
 </template>
